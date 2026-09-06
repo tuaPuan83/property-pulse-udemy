40e0cc32b8c7012ff0e89aaf9b4a5678f8f8c849
Surachai Junphet <williamklaiwonpen@gmail.com>
2026-09-06 10:45:14 +0700

    Add metadata and assets

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,12 @@
 import { Poppins } from "next/font/google";
 import "@/assets/styles/globals.css";
 
+export const metadata = {
+  title: "Property Pulse",
+  keywords: "rental, property, real estate",
+  description: "Find the perfect rental property",
+};
+
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
