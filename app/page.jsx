98af3397b588f965ea652900b60cbe3eac79825b
Surachai Junphet <williamklaiwonpen@gmail.com>
2026-09-06 12:26:49 +0700

    create file based routing

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,14 @@
+import Link from "next/link";
+
 const HomePage = () => {
-  return <div className="text-2xl">Homepage Property pulse</div>;
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl">Welcome Homepage</h1>
+        <Link href="/properties">Go to Properties</Link>
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
---
+++ app/properties/add/page.jsx
@@ -0,0 +1,5 @@
+const AddPropertyPage = () => {
+  return <div>Add property Page</div>;
+};
+
+export default AddPropertyPage;
