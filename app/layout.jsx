ffa0d1da241e8a497cb4e12f24d8cf6945350cfc
Surachai Junphet <williamklaiwonpen@gmail.com>
2026-09-06 21:55:48 +0700

    Create Property Pulse Navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 export const metadata = {
   title: "Property Pulse",
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={poppins.className}>
+        <Navbar />
         <main>{children}</main>
       </body>
     </html>
