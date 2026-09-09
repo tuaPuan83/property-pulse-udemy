1c25d5311957bb73b4b642de7b694307292da271
Surachai Junphet <williamklaiwonpen@gmail.com>
2026-09-09 22:14:31 +0700

    Build HomeProperties Component

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,14 @@
 import Hero from "@/components/Hero";
+import HomeProperties from "@/components/HomeProperties";
 import InfoBoxes from "@/components/InfoBoxes";
-import Link from "next/link";
 
 const HomePage = () => {
   return (
     <>
       <div>
         <Hero />
-        <InfoBoxes/>
+        <InfoBoxes />
+        <HomeProperties />
       </div>
     </>
   );
